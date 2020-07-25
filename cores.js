5df3c00baa34ecad7b5ae81e469ee29ff4fa6112
Martin Maly <maly@maly.cz>
2020-07-26 00:49:53 +0200

    Request is deprecated; substitued by Got

--- cores.js
+++ cores.js
@@ -1,61 +1,78 @@
 const confpath = require("./config.js");
 const fs = require("fs");
-var request = require('request')
-const colors = require("colors/safe")
+//var request = require("request");
+const got = require("got");
+const colors = require("colors/safe");
 
-const cores = confpath+"cores.json";
+const cores = confpath + "cores.json";
 
-var getCoresFromGithub = function(cb) {
+var getCoresFromGithub = function (cb) {
+  return new Promise(function (resolve, reject) {
+    // Do async job
+    var options = {
+      method: "GET",
+      url: "https://raw.githubusercontent.com/maly/cores/master/cores.json",
+      headers: {
+        "Cache-Control":
+          "no-store, no-cache, must-revalidate, proxy-revalidate",
+        Pragma: "no-cache",
+        Expires: "0",
+        "Surrogate-Control": "no-store",
+      },
+      //json: true,
+    };
 
-    return new Promise(function(resolve, reject) {
-        // Do async job
-        var options = { method: 'GET',
-		    url: 'https://raw.githubusercontent.com/maly/cores/master/cores.json',
-		    headers: { 
-                "Cache-Control": "no-store, no-cache, must-revalidate, proxy-revalidate",
-                "Pragma": "no-cache",
-                "Expires": "0",
-                "Surrogate-Control": "no-store"
-                },
-		    json: true 
-        }
-    
-	    request(options, function (error, response, body) {
-            if (error) reject(error)
-            else {
-                console.log(colors.green("Cores list updated from master"))
-                //console.log(JSON.stringify(body))
-                resolve(body);
-            }
-		    //cb(out)
-	    })
-
-    })
-
-	
-}
+    (async () => {
+      try {
+        const response = await got(options);
+        //console.log("statusCode:", response.statusCode);
+        //console.log("body:", response.body);
+        console.log(colors.green("Cores list was updated from master"));
+        resolve(JSON.parse(response.body));
+      } catch (error) {
+        console.log("error:", error);
+        reject(error);
+      }
+    })();
+    /*
+    request(options, function (error, response, body) {
+      if (error) reject(error);
+      else {
+        console.log(colors.green("Cores list updated from master"));
+        //console.log(JSON.stringify(body))
+        resolve(body);
+      }
+      //cb(out)
+    });
+      */
+  });
+};
 
 if (!fs.existsSync(cores)) {
-    getCoresFromGithub()
-    .then((body) => fs.writeFileSync(cores,JSON.stringify(body)));
+  getCoresFromGithub().then((body) =>
+    fs.writeFileSync(cores, JSON.stringify(body))
+  );
 }
 
-var getList = function() {
-    if (!fs.existsSync(cores)) {
-        console.log(colors.red("No list. Perform `cores u` first."))
-    return[];
-    }
-    return JSON.parse(fs.readFileSync(cores))
-}
+var getList = function () {
+  if (!fs.existsSync(cores)) {
+    console.log(colors.red("No list. Perform `cores u` first."));
+    return [];
+  }
+  return JSON.parse(fs.readFileSync(cores));
+};
 
-var getCore = function(core) {
-    var c = JSON.parse(fs.readFileSync(cores));
-    //console.log(c)
-    return c.filter((f)=>(f.name===core))
-}
+var getCore = function (core) {
+  var c = JSON.parse(fs.readFileSync(cores));
+  //console.log(c)
+  return c.filter((f) => f.name === core);
+};
 
 module.exports = {
-    forceUpdate:() => getCoresFromGithub().then((body) => fs.writeFileSync(cores,JSON.stringify(body))),
-    getList:getList,
-    getCore:getCore
-}
+  forceUpdate: () =>
+    getCoresFromGithub().then((body) =>
+      fs.writeFileSync(cores, JSON.stringify(body))
+    ),
+  getList: getList,
+  getCore: getCore,
+};
